84bd92b0d8d940ebb61652e8a00027bbc6898881
BochenChleba <null>
2022-10-18 15:43:33 +0200

    ➕ added paper and basic header

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Text, View } from "react-native"
+import { View } from "react-native"
 
-export default function App() {
+import { Appbar, DarkTheme, Provider as PaperProvider, withTheme } from "react-native-paper"
+
+const theme = DarkTheme
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+        <Appbar.Header>
+          <Appbar.Content title="Nexum" />
+          <Appbar.Action icon="raspberry-pi" onPress={() => { }} />
+          <Appbar.Action icon="account-multiple" onPress={() => { }} />
+        </Appbar.Header>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  }
-})
+export default App
