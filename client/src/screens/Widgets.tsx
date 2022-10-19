3439eaf65733c336c23273e93a0346981a12ce44
BochenChleba <null>
2022-10-19 14:52:18 +0200

    💄 added basic tabs setup on widgets screen

--- client/src/screens/Widgets.tsx
+++ client/src/screens/Widgets.tsx
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native"
+import { useTheme } from "react-native-paper"
+
+import { Tabs, TabScreen } from 'react-native-paper-tabs'
 
 function Widgets() {
+  const theme = useTheme()
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Widgets</Text>
-    </View>
+    <Tabs mode="scrollable">
+      {[...Array(5)].map((_, i) => (
+        <TabScreen label={"Tab " + i}>
+          <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+            <Text style={{ color: "#fff" }}>tab {i}</Text>
+          </View>
+        </TabScreen>
+      ))}
+    </Tabs>
   )
 }
 
