824fea893b43917bc4dd27609038f6e2b7dcf512
BochenChleba <null>
2022-10-27 17:15:22 +0200

    💄 added basic Unknown widget design

--- client/src/components/Tab/Widgets/Unknown/Unknown.tsx
+++ client/src/components/Tab/Widgets/Unknown/Unknown.tsx
@@ -1,7 +1,47 @@
-import { Text } from "react-native-paper"
+import { View, StyleSheet } from "react-native"
+import { Text, List, useTheme, Theme } from "react-native-paper"
+import { WidgetData } from "../types"
 
-function Unknown() {
-  return <Text>Unknown Widget</Text>
+function Unknown({ type }: WidgetData) {
+  const theme = useTheme()
+  const styles = getStyles(theme)
+
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.container}>
+        <List.Icon style={styles.icon} icon="alert" color="#565656" />
+        <Text
+          style={styles.text}
+        >{`There is no\nwidget of "${type}" type`}</Text>
+      </View>
+    </View>
+  )
 }
 
 export default Unknown
+
+const getStyles = (theme: Theme) => {
+  return StyleSheet.create({
+    wrapper: {
+      flex: 1,
+      padding: 5
+    },
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      borderColor: "#565656",
+      borderWidth: 3,
+      borderStyle: "dashed",
+      padding: 10
+    },
+    icon: {
+      margin: 0
+    },
+    text: {
+      color: "#565656",
+      textAlign: "center",
+      fontWeight: "bold"
+    }
+  })
+}
