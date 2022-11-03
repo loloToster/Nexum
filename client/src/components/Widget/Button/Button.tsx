1952cd1471b274a75d8e5b8377c462d60dde1455
BochenChleba <null>
2022-11-03 11:43:29 +0100

    ✨ created basic Button Widget

--- client/src/components/Widget/Button/Button.tsx
+++ client/src/components/Widget/Button/Button.tsx
@@ -1,7 +1,71 @@
-import { Text } from "react-native-paper"
+import { useState } from "react"
+import {
+  View,
+  TouchableWithoutFeedback,
+  StyleSheet,
+  ColorValue,
+  GestureResponderEvent
+} from "react-native"
+import { Text, useTheme, Theme } from "react-native-paper"
 
-function Button(props) {
-  return <Text>{"Button: " + JSON.stringify(props)}</Text>
+import { WidgetData } from "../types"
+
+function Button(props: WidgetData) {
+  const color = "teal"
+  const text = "Button"
+  const isSwitch = false
+
+  const theme = useTheme()
+  const styles = getStyles(theme, color)
+
+  const [active, setActive] = useState(false)
+
+  type PressFunc = ((event: GestureResponderEvent) => void) | undefined
+
+  let pressHandler: PressFunc,
+    pressInHandler: PressFunc,
+    pressOutHandler: PressFunc
+
+  if (isSwitch) {
+    pressHandler = () => setActive(prev => !prev)
+  } else {
+    pressInHandler = () => setActive(true)
+    pressOutHandler = () => setActive(false)
+  }
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={pressHandler}
+      onPressIn={pressInHandler}
+      onPressOut={pressOutHandler}
+    >
+      <View
+        style={{
+          ...styles.container,
+          backgroundColor: active ? color : "transparent"
+        }}
+      >
+        <Text style={styles.text}>{text}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  )
 }
 
 export default Button
+
+const getStyles = (theme: Theme, color: ColorValue) => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      borderColor: color,
+      borderWidth: 3,
+      margin: 5,
+      borderRadius: theme.roundness
+    },
+    text: {
+      fontSize: 20
+    }
+  })
+}
