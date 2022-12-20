08bf98bf4567ad0584945c095dcca9aae8d4eca2
loloToster <lolo.toster4@gmail.com>
2022-12-20 13:52:19 +0100

    ✨ added keep alive feature

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core"
 import { ValidationPipe } from "@nestjs/common"
+import axios from "axios"
 
 import { AppModule } from "./app.module"
 
@@ -12,9 +13,19 @@ async function bootstrap() {
   app.useGlobalInterceptors(new NotFoundInterceptor())
 
   const port = process.env.PORT || 3000
+
   await app.listen(port, "0.0.0.0", () => {
     // eslint-disable-next-line no-console
     console.log("listening on port:", port)
+
+    if (process.env.KEEP_ALIVE_URL) {
+      // eslint-disable-next-line no-console
+      console.log(`keeping ${process.env.KEEP_ALIVE_URL} alive`)
+
+      setInterval(() => {
+        axios.get(process.env.KEEP_ALIVE_URL).catch(() => null)
+      }, 10 * 60 * 1000)
+    }
   })
 }
 
