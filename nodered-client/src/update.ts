e06bcecf6a0f106b633ae6b7f807292a5434268f
loloToster <lolo.toster4@gmail.com>
2023-01-10 17:01:16 +0100

    🥅 added better error handling

--- nodered-client/src/update.ts
+++ nodered-client/src/update.ts
@@ -11,24 +11,22 @@ export = function (RED: nodered.NodeAPI) {
 
       this.device = RED.nodes.getNode(config.device) as DeviceConfigNode | null
 
-      this.on("input", (msg: nodered.NodeMessage) => {
+      this.on("input", (msg, send, done) => {
         const customId = config.customId || msg.topic
 
         if (typeof customId !== "string") {
-          this.error("customId cannot be undefined")
+          done(new Error("Custom Id cannot be undefined"))
           return
         }
 
         const value = msg.payload
 
         if (
-          typeof value !== "string" ||
-          typeof value !== "number" ||
+          typeof value !== "string" &&
+          typeof value !== "number" &&
           typeof value !== "boolean"
         ) {
-          this.error(
-            "type of values/msg.payload can only be a string, number or boolean"
-          )
+          done(new Error("Value can only be a string, number or boolean"))
           return
         }
 
