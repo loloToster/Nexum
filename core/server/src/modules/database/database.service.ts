2752cb2a4fac1aa9f315df29759d05563d5bf389
loloToster <lolo.toster4@gmail.com>
2023-01-18 16:41:27 +0100

    ✨ create an admin if no user in db has admin privilages

--- core/server/src/modules/database/database.service.ts
+++ core/server/src/modules/database/database.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, Logger } from "@nestjs/common"
 import { PrismaClient } from "@prisma/client"
 
 @Injectable()
 export class DatabaseService extends PrismaClient {
+  private readonly logger = new Logger(DatabaseService.name)
+
   constructor() {
     super()
+
+    this.user
+      .findFirst({ where: { isAdmin: true } })
+      .then(async user => {
+        if (user) return
+
+        const initAdmin = await this.user.create({
+          data: { isAdmin: true, name: "admin", id: "admin" }
+        })
+
+        this.logger.log(`Created initial admin with id: '${initAdmin.id}'`)
+      })
+      .catch(err => {
+        this.logger.warn("Error while creating initial admin: " + err)
+      })
   }
 }
