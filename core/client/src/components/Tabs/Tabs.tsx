4771d691003a0ac0e73c4d054ac60a9afaa67e3a
loloToster <lolo.toster4@gmail.com>
2023-01-19 14:44:20 +0100

    🚸 handle none tabs and only one tab

--- core/client/src/components/Tabs/Tabs.tsx
+++ core/client/src/components/Tabs/Tabs.tsx
@@ -1,18 +1,51 @@
 import React from "react"
+import { View, StyleSheet } from "react-native"
+
+import { List, Text, Theme, useTheme } from "react-native-paper"
 import {
   Tabs as PaperTabs,
   TabScreen as PaperTabScreen
 } from "react-native-paper-tabs"
 
-import Tab from "src/components/Tab/Tab"
 import { TabData } from "src/types"
 
+import { useUser } from "src/contexts/user"
+
+import Translatable from "src/components/Translatable/Translatable"
+import Tab from "src/components/Tab/Tab"
+
 function Tabs({ data }: { data: TabData[] }) {
-  return (
+  const theme = useTheme()
+  const styles = getStyles(theme)
+
+  const { user } = useUser()
+
+  return !data.length ? (
+    <Translatable>
+      <View style={styles.noTabsWrapper}>
+        <Text style={styles.noTabs}>
+          {user?.isAdmin ? (
+            <>
+              You don't have access to any tabs. You can add additional
+              permissions on Users tab by clicking:
+              <List.Icon style={styles.icon} icon="account-multiple" />
+            </>
+          ) : (
+            <>
+              You don't have access to any tabs. Ask an administrator for
+              additional permissions.
+            </>
+          )}
+        </Text>
+      </View>
+    </Translatable>
+  ) : data.length === 1 ? (
+    <Tab widgets={data[0].widgets} />
+  ) : (
     <PaperTabs mode="scrollable">
       {data.map((tab, i) => (
         <PaperTabScreen key={i} label={tab.name}>
-          <Tab widgets={tab.widgets}></Tab>
+          <Tab widgets={tab.widgets} />
         </PaperTabScreen>
       ))}
     </PaperTabs>
@@ -20,3 +53,24 @@ function Tabs({ data }: { data: TabData[] }) {
 }
 
 export default Tabs
+
+const getStyles = (theme: Theme) => {
+  return StyleSheet.create({
+    noTabsWrapper: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    noTabs: {
+      fontSize: 20,
+      padding: 40,
+      textAlign: "center"
+    },
+    icon: {
+      margin: 0,
+      height: 30,
+      width: 30
+    }
+  })
+}
